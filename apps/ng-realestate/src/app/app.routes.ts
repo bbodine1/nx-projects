28ef72f3d8fbbd77af916e09c4e40d8f9d0b20d9
Brad <bbodine1@gmail.com>
2023-10-12 21:36:46 -0500

    add app routes

--- apps/ng-realestate/src/app/app.routes.ts
+++ apps/ng-realestate/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },
+];
